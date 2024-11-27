874ef5892086152bcb3d2ec414ce0e6a364b8e20
usama_forayaje <usamaforayaje03@gmail.com>
2024-11-27 20:59:49 +0600

    Fix JSON server module issue

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
-const jsonServer = require("json-server");
-const cors = require("cors");
+import jsonServer from "json-server";
+import cors from "cors";
 
 const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // আপনার ডেটার সোর্স
+const router = jsonServer.router("./src/database/db.json");
 const middlewares = jsonServer.defaults();
 
-server.use(cors()); // CORS এনাবল
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 
-const PORT = process.env.PORT || 4000; // Render-এ PORT ব্যবহার করা বাধ্যতামূলক
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
 });
